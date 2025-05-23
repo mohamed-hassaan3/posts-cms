738537f5e2b581ec165b2904dcaa3fa26220adbc
Mohamed Hassaan <mmhassaan3@gmail.com>
2025-05-23 22:10:00 +0400

    fetch data from Sanity and made only posts

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,7 +1,13 @@
+import { sanityFetch } from '@/sanity/lib/live';
+import { QUERY_POST } from '@/sanity/lib/queries';
 import React from 'react'
 
 const page = async ({params}: {params: {slug: string}}) => {
     const { slug } = params;
+    const post = await sanityFetch({
+        query: QUERY_POST,
+        
+    })
   return (
     <div>
       Slug: {slug}
--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { sanityFetch } from "@/sanity/lib/live";
+import { QUERY_POSTS } from "@/sanity/lib/queries";
+import Link from "next/link";
+import React from "react";
 
-const page = () => {
+const page = async () => {
+  const { data: posts } = await sanityFetch({ query: QUERY_POSTS });
+  console.log(posts);
   return (
-    <div>
-      Posts
-    </div>
-  )
-}
+    <main className="flex flex-col p-4">
+      <button className="py-1 px-4 w-fit bg-gray-400 text-black text-lg font-thin rounded-md">
+        <Link href={`/`}>Back</Link>
+      </button>
+      <article className="my-12 space-y-6">
+        <h1 className="text-xl font-semibold">Index of Posts</h1>
+        <ul className="grid md:grid-cols-2 grid-cols-1 gap-x-16 gap-y-6">
+          {posts &&
+            posts.map((post) => (
+              <li
+                key={post._id}
+                className="px-2 py-4 bg-amber-100 rounded-sm hover:bg-amber-200 cursor-pointer"
+              >
+                <Link href={`/posts/${post?.slug?.current}`}>{post.title}</Link>
+              </li>
+            ))}
+        </ul>
+      </article>
+    </main>
+  );
+};
 
-export default page
+export default page;
